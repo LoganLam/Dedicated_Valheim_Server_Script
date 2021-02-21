55dd8145199daec34e99a90bd3d47e122a5f6347
LoganLam <43323647+LoganLam@users.noreply.github.com>
2021-02-20 22:05:42 -0500

    Automatically check for and apply updates

--- build_dedicated_valheim_server.sh
+++ build_dedicated_valheim_server.sh
@@ -35,6 +35,13 @@ KillSignal=SIGINT
 WorkingDirectory=/home/steam/valheimserver
 LimitNOFILE=100000
 
+#chown steam user permissions to all of user steam dir location
+tput setaf 1; echo "Setting steam account permissions to /home/steam/*"
+chown -Rf steam:steam /home/steam/*
+tput setaf 2; echo "Done"
+tput setaf 9;
+sleep 1
+
 #add multiverse repo
 tput setaf 2; echo "Adding multiverse REPO"
 add-apt-repository -y multiverse
